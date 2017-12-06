6a8cdfea8f4ef0c56400584aa20be8903186603d
Anna-Marie Cooper <annamarie@rogueminx.com>
2017-12-06 12:11:00 -0800

    JS logic works completely for Player One.

--- js/scripts.js
+++ js/scripts.js
@@ -7,19 +7,34 @@ function playerOne () {
   this.total1 = 0;
 }
 
-
 playerOne.prototype.Random = function() {
   min = 1;
   max = 6;
   this.roll1 = Math.floor(Math.random() * (max - min + 1)) + min;
-  console.log(newPlayerOne.roll1);
   if (this.roll1 === 1) {
     this.score1 = 0;
     console.log("you rolled a one, and lose your points this turn.");
   } else {
     this.score1 += this.roll1;
-    console.log(this.score1);
+    if ((this.score1 + this.total1) < 100) {
+      console.log("Your roll is: " + this.roll1);
+      console.log("Your score this round is: " + this.score1);
+    } else {
+      console.log("Your roll is: " + this.roll1);
+      console.log("That mean's you've reached " + (this.total1 + this.score1) + ", and win the game!");
+    }
   };
+};
+
+playerOne.prototype.Tally = function() {
+  if ((this.score1 + this.total1) < 100) {
+    this.total1 += this.score1;
+    console.log("Your score this round is: " + this.score1 + ", and your total so far is: " + this.total1);
+  } else {
+    console.log("Your roll is: " + this.roll1);
+    console.log("You've reached " + this.total1 + ", and win the game!");
+  }
+  this.score1 = 0;
 };
 
 function playerTwo (roll2, score2, total2) {
@@ -37,6 +52,7 @@ function playerTwo (roll2, score2, total2) {
 //user interface logic
 $(document).ready(function(){
   newPlayerOne = new playerOne();
+  newPlayerTwo = new playerTwo();
   $("#roll01").click(function(event){
     event.preventDefault();
     newPlayerOne.Random();
@@ -44,6 +60,11 @@ $(document).ready(function(){
     console.log("okay great");
 
   });
+  $("#hold01").click(function(event){
+    event.preventDefault();
+    newPlayerOne.Tally();
+  });
+
   // var currentScore = 0;
   // if (roll === 1) {
   //   currentScore = 0;
