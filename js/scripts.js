c129f19c61fdde7f08f697819de1478ab7c7e3b9
Anna-Marie Cooper <annamarie@rogueminx.com>
2017-12-06 13:12:12 -0800

    JS now checks to see if it PlayerOne's turn.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,36 @@
 //back end logic
 
 function playerOne () {
-  this.roll1 = 0;
+  this.roll1 = 1;
   this.score1 = 0;
   this.total1 = 0;
+  this.turn = "yes";
 }
 
 playerOne.prototype.Random = function() {
-  min = 1;
-  max = 6;
-  this.roll1 = Math.floor(Math.random() * (max - min + 1)) + min;
-  if (this.roll1 === 1) {
-    this.score1 = 0;
-    $("#current-roll01").text(this.roll1);
-    $("#current-score01").text(this.score1)
+  if (this.turn === "no"){
+    console.log("It's not your turn!");
   } else {
-    $("#current-roll01").text(this.roll1);
-    this.score1 += this.roll1;
-    if ((this.score1 + this.total1) < 100) {
+    min = 1;
+    max = 6;
+    this.roll1 = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (this.roll1 === 1) {
+      this.score1 = 0;
+      newPlayerTwo.turn = "yes";
+      this.turn = "no";
       $("#current-roll01").text(this.roll1);
       $("#current-score01").text(this.score1)
-      // console.log("Your score this round is: " + this.score1);
     } else {
       $("#current-roll01").text(this.roll1);
-      $("#total-score01").text(this.total1 + this.score1 + "You Win!");
-      // console.log("That mean's you've reached " + (this.total1 + this.score1) + ", and win the game!");
-    }
+      this.score1 += this.roll1;
+      if ((this.score1 + this.total1) < 100) {
+        $("#current-roll01").text(this.roll1);
+        $("#current-score01").text(this.score1)
+      } else {
+        $("#current-roll01").text(this.roll1);
+        $("#total-score01").text(this.total1 + this.score1 + " You Win!");
+      };
+    };
   };
 };
 
@@ -34,26 +39,28 @@ playerOne.prototype.Tally = function() {
     this.total1 += this.score1;
     $("#current-score01").text(this.score1);
     $("#total-score01").text(this.total1)
-    // console.log("Your score this round is: " + this.score1 + ", and your total so far is: " + this.total1);
   } else {
     $("#current-roll01").text(this.roll1);
-    $("#total-score01").text((this.total1 + this.roll) + "YOU WIN!")
-    //console.log("You've reached " + this.total1 + ", and win the game!");
+    $("#total-score01").text((this.total1 + this.roll) + " You Win!")
   }
   this.score1 = 0;
+  this.roll1 = 0;
+  newPlayerTwo.turn = "yes";
+  this.turn = "no";
 };
 
-function playerTwo (roll2, score2, total2) {
-  this.roll2 = roll2;
-  this.score2 = score2;
-  this.total2 = total2;
+playerOne.prototype.Playability = function() {
+  if (this.roll1 === 0){
+    console.log("It's not your turn!");
+  };
+};
+
+function playerTwo () {
+  this.roll2 = 1;
+  this.score2 = 0;
+  this.total2 = 0;
+  this.turn = "no";
 }
-
-
-
-
-
-
 
 //user interface logic
 $(document).ready(function(){
@@ -61,21 +68,12 @@ $(document).ready(function(){
   newPlayerTwo = new playerTwo();
   $("#roll01").click(function(event){
     event.preventDefault();
+    // newPlayerOne.Playability();
     newPlayerOne.Random();
-
     console.log("okay great");
-
   });
   $("#hold01").click(function(event){
     event.preventDefault();
     newPlayerOne.Tally();
   });
-
-  // var currentScore = 0;
-  // if (roll === 1) {
-  //   currentScore = 0;
-  // } else {
-  //   newScore = new Score(roll);
-  // }
-
 });
